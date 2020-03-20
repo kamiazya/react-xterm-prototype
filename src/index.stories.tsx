f9e0f069245e82d142536ac5e4c93c986263cd1b
kamiazya <yuki@kamiazya.tech>
2020-03-20 13:58:32 +0900

    add storybook

---
+++ src/index.stories.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+import ExampleComponent from './index';
+
+export default {
+  title: 'Components',
+};
+
+// tslint:disable-next-line: variable-name
+export const Example = () => <ExampleComponent text="X" />;
