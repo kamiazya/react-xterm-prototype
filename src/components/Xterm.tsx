5ade3e51a2b7c4c99c1784d533bb73c76f622b5e
kamiazya <yuki@kamiazya.tech>
2020-03-22 22:55:47 +0900

    remove XtermConainer

--- src/components/Xterm.tsx
+++ src/components/Xterm.tsx
@@ -1,13 +1,19 @@
 // tslint:disable: variable-name
 import * as React from 'react';
 import * as xterm from 'xterm';
-import { XtermContainer } from './XtermContainer';
 import { useTerminal } from '../hooks/use-terminal';
 
 export const Xterm = React.forwardRef((_, ref: React.Ref<xterm.Terminal>) => {
   const terminal = useTerminal();
   React.useImperativeHandle(ref, () => terminal, [terminal]);
-  return <XtermContainer terminal={terminal} />;
+  const divRef = React.createRef<HTMLDivElement>();
+  React.useEffect(() => {
+    terminal.open(divRef.current!);
+    return () => {
+      terminal.dispose();
+    };
+  }, [terminal]);
+  return <div ref={divRef} />;
 });
 
 Xterm.displayName = 'Xterm';
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 import { TerminalContext } from './contexts/TerminalContext';
-import { XtermContainer } from './components/XtermContainer';
 import { Xterm } from './components/Xterm';
 import { useTerminal } from './hooks/use-terminal';
-export { XtermContainer, Xterm, TerminalContext, useTerminal };
+export { Xterm, TerminalContext, useTerminal };
 export default Xterm;
