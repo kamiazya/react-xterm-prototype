b77fb9a2e4c402fe71c45c991f2e1aca8280d3ff
kamiazya <yuki@kamiazya.tech>
2020-03-22 20:12:57 +0900

    improve

--- src/components/Xterm.tsx
+++ src/components/Xterm.tsx
@@ -4,11 +4,7 @@ import * as xterm from 'xterm';
 import { XtermContainer } from './XtermContainer';
 import { useTerminal } from '../hooks/use-terminal';
 
-type Props = {
-  // options?: xterm.ITerminalOptions;
-};
-
-export const Xterm = React.forwardRef((_: Props, ref: React.Ref<xterm.Terminal>) => {
+export const Xterm = React.forwardRef((_, ref: React.Ref<xterm.Terminal>) => {
   const terminal = useTerminal();
   React.useImperativeHandle(ref, () => terminal, [terminal]);
   return <XtermContainer terminal={terminal} />;
--- src/components/XtermContainer.tsx
+++ src/components/XtermContainer.tsx
@@ -1,6 +1,7 @@
 // tslint:disable: variable-name
 import * as React from 'react';
 import * as xterm from 'xterm';
+import * as PropTypes from 'prop-types';
 
 type Props = {
   terminal: xterm.Terminal;
@@ -10,9 +11,15 @@ export const XtermContainer: React.FC<Props> = ({ terminal }) => {
   const divRef = React.createRef<HTMLDivElement>();
   React.useEffect(() => {
     terminal.open(divRef.current!);
-    return () => terminal.dispose();
+    return () => {
+      terminal.dispose();
+    };
   }, [terminal]);
   return <div ref={divRef} />;
 };
 
 XtermContainer.displayName = 'XtermContainer';
+
+XtermContainer.propTypes = {
+  terminal: PropTypes.instanceOf(xterm.Terminal).isRequired,
+};
